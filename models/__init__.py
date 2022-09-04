c12c18cc208855270262383575ad4b64bd86e467
sheilaowiso <sheilajoylasoi@gmail.com>
2022-09-04 13:48:38 -0700

    AirBnB_clone

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Saves objects in file to FileStorage class attribute __objects"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
